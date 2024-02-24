22d0499da22d5f84279f7764a580cff4f0bd2c6c
pimmimagi <sarocha.sit@ku.th>
2024-02-24 23:15:48 +0700

    Set videos database

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
+import firebase_admin
 from fastapi import FastAPI
+from decouple import config
+from firebase_admin import credentials, firestore
+
+
+cred = credentials.Certificate(config('CRED'))
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"Set up API"}
+
+@app.get("/videos")
+def videos():
+    videos_ref = db.collection("videos")
+    docs = videos_ref.stream()
+
+    video_list = []
+    for doc in docs:
+        video_list.append(doc.to_dict())
+    return video_list
